6ff19b0ede4609a1ad7e04d6147b14db56d6ed87
Garvit <112452637+garvitjava@users.noreply.github.com>
2025-02-23 00:01:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -269,8 +269,8 @@ async def predict_emotion_api(file: UploadFile = File(...)):
     os.remove(video_path)
     
     return emotions
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=PORT)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=PORT)
 
 
 
